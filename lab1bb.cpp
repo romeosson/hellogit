5dc80015bf958ebb64e52b4363aebd1dda593df5
romeosson <romeosson@users.noreply.github.com>
2019-02-21 17:58:02 -0400

    lab 1

---
+++ lab1bb.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std
+int main()
+{
+	string name="Michael";
+	string name2=name+"Ellis";
+	vector <int> vec={1,2,3,4,5,6,7,8,9,10};
+	vector <char> veca;
+	
+	vec.pop_back(10);
+	veca.push_back('a','b','c','d','e','f','g');
+	
+	for (int d=0; d<3;d++)
+	{
+		veca.push_back(m);
+	}
+	
+	cout<< veca<<" ";
+	cout<< vec<< endl;
+	
+	cout<<"First name"<<name<<endl;
+	cout<<"Full name"<<name2<<endl;
+	
+	system"pause";
+}
